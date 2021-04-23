6515b485b00d393a82727b3369743cd28556cae5
rabiatul-adawiyah25 <rabiatuladawiyahhs@gmail.com>
2021-04-24 03:45:50 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Rabiatul Adawiyah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
